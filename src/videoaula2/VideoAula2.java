950ae88bd86edff9fdb645b10eb1945ce6b9b6a0
Absyd <allaph_anderson@hotmail.com>
2017-04-29 12:17:55 -0300

    java1

---
+++ src/videoaula2/VideoAula2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoaula2;
+
+/**
+ *
+ * @author absyd
+ */
+public class VideoAula2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá Mundo");//comando de escrita em tela "modo dos"
+    }
+    
+}
